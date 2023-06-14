93eb89d709e49c353d088c05e9ef96e82ae69691
Grillo PC <carlsenrmt26@gmail.com>
2023-06-14 00:02:45 -0400

    Edit form grilloPc

--- resources/views/pages/turno/turno-edit.blade.php
+++ resources/views/pages/turno/turno-edit.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
     <x-slot name="header_content">
-        <h1>{{ __('Editar Turno') }}</h1>
-
+        <h1>{{ __('Editar Turno') }}         
+            <small><a href="https://demo.backpackforlaravel.com/admin/user" class="d-print-none font-sm"><i class="fa fa-angle-double-left"></i>Back to all </small>
+        </h1>
+        
         <div class="section-header-breadcrumb">
         <div class="breadcrumb-item active"><a href="{{ route('dashboard') }}">Dashboard</a></div>
             <div class="breadcrumb-item"><a href="#">Turno</a></div>
--- resources/views/vendor/jetstream/components/form-section.blade.php
+++ resources/views/vendor/jetstream/components/form-section.blade.php
@@ -1,14 +1,10 @@
 @props(['submit'])
 
 <div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-3 md:gap-6']) }}>
-    <x-jet-section-title>
-        <x-slot name="title">{{ $title }}</x-slot>
-        <x-slot name="description">{{ $description }}</x-slot>
-    </x-jet-section-title>
-
+    
     <div class="mt-5 md:mt-0 md:col-span-2">
         <form wire:submit.prevent="{{ $submit }}">
-            <div class="shadow overflow-hidden sm:rounded-md">
+            <div class=" overflow-hidden sm:rounded-md">
                 <div class="px-4 py-5 bg-white sm:p-6">
                     <div class="grid grid-cols-6 gap-6">
                         {{ $form }}
@@ -23,4 +19,8 @@
             </div>
         </form>
     </div>
+    <x-jet-section-title>
+        <x-slot name="title">{{ $title }}</x-slot>
+        <x-slot name="description">{{ $description }}</x-slot>
+    </x-jet-section-title>
 </div>
